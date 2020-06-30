f3f69b5284312e088591dd5ddb9b71959904ea76
ankit4b <64839430+ankit4b@users.noreply.github.com>
2020-06-30 10:33:04 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '4$3z1@kmd^qcgi8)a+w4snrugto_p(30ae=05q-%+znt8#7=qu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://healthinsuranceprediction.herokuapp.com/','*']
 
 
 # Application definition
